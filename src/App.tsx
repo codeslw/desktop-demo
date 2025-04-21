848bb3f7215eedf7cf2f64ea5d428a9fc6926532
codeslw <sanvarkhujaev@gmail.com>
2025-04-21 15:07:20 +0500

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import { Homescreen } from './components/templates/Homescreen';
 import './assets/styles/global.css';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { BrowserRouter } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
+
+
+
   return (
     <BrowserRouter>
       <ThemeProvider>
--- src/components/templates/Homescreen.tsx
+++ src/components/templates/Homescreen.tsx
@@ -4,10 +4,17 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DesktopProvider } from '../../contexts/DesktopContext';
 import { WindowProvider } from '../../contexts/WindowContext';
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import { WindowManager } from './WindowManager';
+import { useNavigate } from 'react-router-dom';
 
 export const Homescreen = memo(() => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/');
+  }, []);
+  
   return (
     <DndProvider backend={HTML5Backend}>
       <DesktopProvider>
